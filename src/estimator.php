c8be1df7ceff43b82be12eb60f7d9301f2a79116
nserekoallan <51728593+nserekoallan@users.noreply.github.com>
2020-04-14 22:47:22 +0300

    check json

--- src/estimator.php
+++ src/estimator.php
@@ -4,7 +4,9 @@ include_once "severeImpact.php";
 
 function covid19ImpactEstimator($data)
 {
-  // $data = json_decode($data);
+  
+  $data = json_encode($data);
+  $data = json_decode($data);
 
   //normalise duration
   $duration = 0;
